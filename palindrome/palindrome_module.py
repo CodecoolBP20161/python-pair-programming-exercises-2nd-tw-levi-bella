9ff33b1cf9d922e0058f447ea909ac8f7f469550
matheisson <csanyil@gmail.com>
2016-05-30 14:28:45 +0200

    palindrome

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,18 @@
-def palindrome(str):
-    return
+def palindrome(pal):
+	pal = pal.lower()
+	pal = pal.replace(" ", "")
+	if pal == pal[::-1]:
+		return True
+	else:
+		print("false")
+		return False
 
 
 def main():
-    return
+	pal = str(input("Say something: "))
+	palindrome(pal)
+	return
 
 
 if __name__ == '__main__':
-    main()
+	main()
