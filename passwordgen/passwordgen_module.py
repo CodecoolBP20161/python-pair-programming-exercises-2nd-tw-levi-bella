d4fc5f2ac277e9a38753e3c25fcd0811d65ccb6a
matheisson <csanyil@gmail.com>
2016-05-31 09:30:20 +0200

    shuffles

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,18 +1,35 @@
 import random
+# chars = ("!@$%^&*()#?", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789")
+# index = random.randint(0, len(chars)-1)
+# index2 = random.randint(0, len(chars[index])-1)
+# password.append(chars[index][index2])
 
+a=("!@$%^&*()#?")
+b=("abcdefghijklmnopqrstuvwxyz")
+c=("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+d= ("0123456789")
 def passwordgen():
-	chars = ("!@$%^&*()#?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	password = []
 	while len(password) != 8:
-		index = random.randint(1, len(chars)-1)
-		password.append(chars[index])
+		for i in range(2):
+			index = random.randint(0, len(a)-1)
+			password.append(a[index])
+		for i in range(2):
+			index = random.randint(0, len(b)-1)
+			password.append(b[index])
+		for i in range(2):
+			index = random.randint(0, len(c)-1)
+			password.append(c[index])
+		for i in range(2):
+			index = random.randint(0, len(d)-1)
+			password.append(d[index])
+	random.shuffle(password)
 	password = "".join(password)
 	return password
 
 
 def main():
-	passwordgen()
-	#print(password)
+	print(passwordgen())
 	return
 
 
