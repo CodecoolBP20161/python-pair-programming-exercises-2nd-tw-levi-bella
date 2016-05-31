dae1e63be15efefd1d679c68e6440c3a277fa839
matheisson <csanyil@gmail.com>
2016-05-31 09:36:24 +0200

    done

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,16 +1,15 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
+
 def listoverlap(a, b):
-	c = sorted(list(set(a).intersection(set(b))))
-	print(a, b)
-	print(c)
-	return c
+    c = sorted(list((set(a) & (set(b)))))
+    return c
 
 
 def main():
-	listoverlap(a,b)
-	return
+    print(listoverlap(a, b))
+    return
 
 
 if __name__ == '__main__':
