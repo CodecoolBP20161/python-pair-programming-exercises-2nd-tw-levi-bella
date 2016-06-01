d0fff222da782818ae3ca7946b2c9ad74c570c93
matheisson <csanyil@gmail.com>
2016-06-01 09:24:01 +0200

    ready

--- years/years_module.py
+++ years/years_module.py
@@ -2,20 +2,21 @@ import datetime
 
 
 def years(age):
-	year = 2016
-	global result
-	result = (year - age) + 100
-	return result
+    year = 2016
+    global result
+    result = (year - age) + 100
+    return result
 
 
 def main():
-	age = 0
-	name = input("What's Your name? ")
-	age = int(input("How old are you?"))
-	years(age)
-	this_many = int(input("How many times do you want it to print?"))
-	print(("Hello ",name,"You'll be 100 yrs old in the year of: ", result, "\n")*this_many)
-	return age
+    age = 0
+    name = input("What's Your name? ")
+    age = int(input("How old are you?"))
+    years(age)
+    this_many = int(input("How many times do you want it to print? "))
+    for i in range(this_many):
+        print("Hello ", name, "You'll be 100 yrs old in the year of: ", result, "\n")
+    return age
 
 
 if __name__ == '__main__':
