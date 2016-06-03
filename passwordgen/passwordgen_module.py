45d96c3b96f47336a2bef7a8982e1aecc184c8bf
matheisson <csanyil@gmail.com>
2016-06-03 09:25:36 +0200

    ready

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -6,20 +6,60 @@ c = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 d = ("0123456789")
 
 
+a = ("!@$%^&*()#?")
+b = ("abcdefghijklmnopqrstuvwxyz")
+c = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+d = ("0123456789")
+
+
+# 2 characters from all 4 lists
 def passwordgen():
     password = []
     while len(password) != 8:
         for i in range(2):
-            index = random.randint(0, len(a) - 1)
+            index = random.randint(0, len(a)-1)
             password.append(a[index])
         for i in range(2):
-            index = random.randint(0, len(b) - 1)
+            index = random.randint(0, len(b)-1)
             password.append(b[index])
         for i in range(2):
-            index = random.randint(0, len(c) - 1)
+            index = random.randint(0, len(c)-1)
             password.append(c[index])
         for i in range(2):
-            index = random.randint(0, len(d) - 1)
+            index = random.randint(0, len(d)-1)
+            password.append(d[index])
+    random.shuffle(password)
+    password = ''.join(password)
+    return password
+
+
+# lower- or uppercase letters
+def weak():
+    password = []
+    while len(password) != 8:
+        for i in range(4):
+            index = random.randint(0, len(c)-1)
+            password.append(c[index])
+        for i in range(4):
+            index = random.randint(0, len(b)-1)
+            password.append(b[index])
+    random.shuffle(password)
+    password = "".join(password)
+    return password
+
+
+# lower+upper & 2 digits
+def medium():
+    password = []
+    while len(password) != 8:
+        for i in range(3):
+            index = random.randint(0, len(b)-1)
+            password.append(b[index])
+        for i in range(3):
+            index = random.randint(0, len(c)-1)
+            password.append(c[index])
+        for i in range(2):
+            index = random.randint(0, len(d)-1)
             password.append(d[index])
     random.shuffle(password)
     password = "".join(password)
@@ -27,7 +67,13 @@ def passwordgen():
 
 
 def main():
-    print(passwordgen())
+    diff = input("Choose password security! => hard/med/weak: ")
+    if diff == "hard":
+        print(passwordgen())
+    if diff == "med":
+        print(medium())
+    if diff == "weak":
+        print(weak())
     return
 
 
